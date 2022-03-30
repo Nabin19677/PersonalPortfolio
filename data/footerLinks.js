1c6e64840563a0af27725b72c9ec3a35beb7b95f
Anil Khadka <anil.khadka3777@gmail.com>
2022-03-30 12:25:02 +0545

    first

---
+++ data/footerLinks.js
@@ -0,0 +1,14 @@
+const links = [
+  {
+    label: "Github",
+    icon: "github",
+    url: "https://github.com/Nabin19677",
+  },
+  {
+    label: "Linkedin",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/nabin19677/",
+  },
+];
+
+export default links;
