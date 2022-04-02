24efcf3741dd61b2d6a875337162f4b31e91c84a
Anil Khadka <anil.khadka3777@gmail.com>
2022-04-03 00:33:06 +0545

    added multi exp

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -1,7 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
 export default function Contact() {
   return (
-    <>
+    <Box>
       <p>Contact Page</p>
-    </>
+    </Box>
   );
 }
--- components/sections/Experience.js
+++ components/sections/Experience.js
@@ -7,9 +7,30 @@ import {
   Container,
   Text,
   Badge,
+  Button,
 } from "@chakra-ui/react";
+import { useState } from "react";
+
+const experiences = [
+  {
+    jobTitle: "MEAN Stack Developer",
+    company: "Chaitanya Design",
+    points: [
+      "Development of Single Page Web Application using Angular 2+.",
+      "Develop REST API Endpoints / Backend Development with Nodejs and MongoDB Database.",
+      "Collaborate with peers on designs, code reviews, and testing.",
+      "Technologies Used: [Node.js, Express.js, Angular, MongoDB, MySQL, JSON]",
+    ],
+  },
+];
 
 export default function Experience() {
+  const [experienceIndex, setExperienceIndex] = useState(0);
+
+  const onCompanyButton = (i) => {
+    setExperienceIndex(i);
+  };
+
   return (
     <Box p="16" id="experience">
       <Center>
@@ -20,18 +41,27 @@ export default function Experience() {
           <Box>
             <HStack>
               <VStack>
-                <Badge colorScheme="red">Chaitanya Design</Badge>
+                {experiences.map((exp, i) => (
+                  <Button
+                    key={exp.company}
+                    colorScheme="red"
+                    onClick={() => onCompanyButton(i)}
+                  >
+                    {exp.company}
+                  </Button>
+                ))}
               </VStack>
               <Box>
                 <Container>
-                  <Heading>MEAN Stack Developer @ Chaitanya Design</Heading>
-                  <Text>
-                    Development of Single Page Web Application using Angular 2+.
-                    Develop REST API Endpoints / Backend Development with Nodejs
-                    and MongoDB Database. Collaborate with peers on designs,
-                    code reviews, and testing. Technologies Used: [Node.js,
-                    Express.js, Angular, MongoDB, MySQL, JSON]
-                  </Text>
+                  <Heading>
+                    {experiences[experienceIndex].jobTitle} @{" "}
+                    {experiences[experienceIndex].company}
+                  </Heading>
+                  <Container>
+                    {experiences[experienceIndex].points.map((p, i) => (
+                      <Text key={i}>{p}</Text>
+                    ))}
+                  </Container>
                 </Container>
               </Box>
             </HStack>
