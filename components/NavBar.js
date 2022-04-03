24f747263d00aa39ad06781ca2dca3bd87d01e3d
Anil Khadka <anil.khadka3777@gmail.com>
2022-04-03 12:09:21 +0545

    fixed sticky innerindex

--- components/NavBar.js
+++ components/NavBar.js
@@ -65,7 +65,7 @@ const NavButtons = () => {
 
 export default function NavBar() {
   return (
-    <Sticky>
+    <Sticky enabled innerZ={99}>
       <HStack px="4" py="2" bg="white">
         <Logo />
         <Spacer />
